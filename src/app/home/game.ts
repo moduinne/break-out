3472c00114c85bba693cf38a7ed327e19be7c1bc
ethanol190 <cloudygumption@gmail.com>
2020-08-10 14:04:09 +1000

    phaser-ionic-start-point

---
+++ src/app/home/game.ts
@@ -0,0 +1,23 @@
+import * as Phaser from 'phaser';
+
+export class GameScene extends Phaser.Scene {
+
+
+constructor() {
+    super({ key: 'game' });
+    }
+
+
+    preload(){
+        //TODO
+    }
+
+    create(){
+        //TODO
+    }
+
+    update(){
+        //TODO
+    }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import * as Phaser from 'phaser';
+
+import { GameScene } from './game';
+
+const DPR:number = 2;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit, OnDestroy {
+
+  config:Phaser.Types.Core.GameConfig;
+  game:Phaser.Game;
+
+  constructor(private plt:Platform) {
+
+    this.config = {
+
+      type: Phaser.AUTO,
+      width: window.innerWidth * DPR,
+      height: window.innerHeight * DPR,
+      parent: 'gameContainer', //html id
+      scale: {
+        mode:Phaser.Scale.FIT,
+        autoCenter:Phaser.Scale.CENTER_BOTH
+      },
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity : {y:0},
+          debug : false
+        }
+      },
+      scene:GameScene
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.game.destroy(true,false);
+  }
+  ngOnInit(): void {
+    this.game = new Phaser.Game(this.config);
+  }
 
 }
