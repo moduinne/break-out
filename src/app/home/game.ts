e9a3e116c4fdd0e4c19d628d0dd436e76ce9e570
ethanol190 <cloudygumption@gmail.com>
2020-08-10 14:33:15 +1000

    initial breakout commit

--- src/app/home/game.ts
+++ src/app/home/game.ts
@@ -1,19 +1,35 @@
 import * as Phaser from 'phaser';
+const PADDLE_SCALE = 0.25;
+const START_X = innerWidth;
+const START_Y = (2*innerHeight) - 200;
 
 export class GameScene extends Phaser.Scene {
 
+    private paddle: Phaser.Physics.Arcade.Sprite;
 
-constructor() {
-    super({ key: 'game' });
+    constructor() {
+        super({ key: 'game' });
     }
 
 
     preload(){
-        //TODO
+       //paddle image
+       this.load.setPath('/assets/PNG/');
+       this.load.image('paddle', '56-Breakout-Tiles.png');
+        
     }
 
     create(){
-        //TODO
+        //adding paddle to game
+        this.addPaddle();
+    }
+
+     //wrapper method for creating the player/paddle
+    addPaddle(){
+        this.paddle = this.physics.add.sprite(START_X, START_Y, 'paddle').setScale(PADDLE_SCALE).refreshBody();
+        //this.paddle.setImmovable(true);
+        //this.paddle.setCollideWorldBounds(true);
+       // this.paddle.setInteractive();
     }
 
     update(){
